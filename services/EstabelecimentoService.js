c8a7b667ce9c0d5edf9e760a74c81b990843e396
rafareisbr <rafareisbr@gmail.com>
2020-07-19 20:36:54 -0300

    .

--- services/EstabelecimentoService.js
+++ services/EstabelecimentoService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL:
-    'http://pibibox.com.br/api/v1/estabelecimentos/2842a995-f097-43d3-80e8-8870146179c9/',
+    'https://pibibox.com.br/api/v1/estabelecimentos/2842a995-f097-43d3-80e8-8870146179c9/',
   withCredentiais: false,
   headers: {
     Accept: 'application/json',
